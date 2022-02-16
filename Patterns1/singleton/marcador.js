048374833acdb02639de8c7124f73b4886a92c80
xcomi <xaviercomi@gmail.com>
2022-02-16 19:35:55 +0100

    singleton not working

--- Patterns1/singleton/marcador.js
+++ Patterns1/singleton/marcador.js
@@ -1,7 +1,8 @@
 class Marcador {
 
-    constructor(arrayMarcas) {
-        this.arrayMarcas = arrayMarcas;
+    constructor() {
+        this.jugador = jugador;
+        this.puntuacio = puntuacio;
 
         if (typeof Marcador.instance === 'object') {
             return Marcador.instance
